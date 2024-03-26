8faf42b80bd8395834bb154ae515a4c9d8ccf382
yeeunlim <84782741+yeeunlim@users.noreply.github.com>
2024-03-26 15:52:47 +0900

    [Silver II] Title: 에디터, Time: 264 ms, Memory: 41540 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+str = list(input().strip())
+temp = []
+m = int(input())
+for _ in range(m):
+    command = input().split()
+    if command[0] == 'L':
+        if str:
+            temp.append(str.pop())
+    elif command[0] == 'D':
+        if temp:
+            str.append(temp.pop())
+    elif command[0] == 'B':
+        if str:
+            str.pop()
+    elif command[0] == 'P':
+        str.append(command[1])
+print(''.join(str + list(reversed(temp))))
