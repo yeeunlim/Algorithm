55b6e6adabc931219d3627a9bbc14392a94aca28
yeeunlim <84782741+yeeunlim@users.noreply.github.com>
2024-05-22 19:21:02 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,18 @@
+import sys
+
+n, m = map(int, input().split())
+
+array = []  # 현재까지 선택된 요소들을 저장하는 리스트
+def backtracking(start):
+    if len(array) == m:  # 현재까지 선택된 요소의 개수가 m과 같다면
+        print(" ".join(map(str, array)))  # 현재 순열을 출력
+        return  # 함수를 종료하고 돌아감
+
+    for i in range(start, n + 1):  # 1부터 n까지 모든 숫자에 대해
+        if i not in array:  # 현재 선택된 숫자들(array)에 포함되지 않은 숫자 i를 선택
+            array.append(i)  # 숫자 i를 선택 (추가)
+            backtracking(i+1)  # 현재보다 큰 수 선택(i+1)
+            array.pop()  # 선택했던 숫자 i를 제거 (백트래킹)
+
+
+backtracking(1)
