ddbdfd10cc1bfda08ddee23934052da91968c651
Lim Yeeun <84782741+yeeunlim@users.noreply.github.com>
2026-02-10 11:45:23 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.12 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,10 @@
+def solution(num):
+    count = 0
+    while num != 1 and count < 500:
+        if num % 2 == 0:
+            num /= 2
+        else:
+            num = num * 3 + 1
+        count += 1
+        
+    return count if num == 1 else -1
