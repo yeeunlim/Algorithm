89d35eb3445ff010b4a6b4985aa777da013dec9b
Lim Yeeun <84782741+yeeunlim@users.noreply.github.com>
2026-02-20 15:46:38 +0900

    [Silver IV] Title: 큐, Time: 56 ms, Memory: 35076 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -1,26 +1,22 @@
+from collections import deque
 import sys
 input = sys.stdin.readline
-from collections import deque
-queue = deque()
 n = int(input())
-
+que = deque()
 for _ in range(n):
     command = input().split()
     if command[0] == 'push':
-        queue.append(command[1])
+        que.append(int(command[1]))
     elif command[0] == 'pop':
-        if queue:
-            print(queue.popleft())
+        if que:
+            print(que.popleft())
         else:
             print(-1)
     elif command[0] == 'size':
-        print(len(queue))
+        print(len(que))
     elif command[0] == 'empty':
-        if queue: print(0)
-        else: print(1)
+        print(0 if que else 1)
     elif command[0] == 'front':
-        if queue: print(queue[0])
-        else: print(-1)
+        print(que[0] if que else -1)
     elif command[0] == 'back':
-        if queue: print(queue[-1])
-        else: print(-1)
+        print(que[-1] if que else -1)
