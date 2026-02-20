169c68539b38c5634817a1f54a37a7a284f39bf3
Lim Yeeun <84782741+yeeunlim@users.noreply.github.com>
2026-02-20 13:56:58 +0900

    [Silver II] Title: 에디터, Time: 260 ms, Memory: 42644 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -1,20 +1,32 @@
 import sys
+from collections import deque
+
 input = sys.stdin.readline
 
-str = list(input().strip())
-temp = []
+# 초기 문자열을 왼쪽 덱에 담음
+left_deque = deque(input().strip())
+right_deque = deque()
+
 m = int(input())
+
 for _ in range(m):
     command = input().split()
+    
     if command[0] == 'L':
-        if str:
-            temp.append(str.pop())
+        if left_deque:
+            # 왼쪽 끝에서 꺼내 오른쪽 앞에 추가
+            right_deque.appendleft(left_deque.pop())
+            
     elif command[0] == 'D':
-        if temp:
-            str.append(temp.pop())
+        if right_deque:
+            # 오른쪽 앞(커서 바로 옆)에서 꺼내 왼쪽 끝에 추가
+            left_deque.append(right_deque.popleft())
+            
     elif command[0] == 'B':
-        if str:
-            str.pop()
+        if left_deque:
+            left_deque.pop()
+            
     elif command[0] == 'P':
-        str.append(command[1])
-print(''.join(str + list(reversed(temp))))
+        left_deque.append(command[1])
+
+print("".join(left_deque) + "".join(right_deque))
