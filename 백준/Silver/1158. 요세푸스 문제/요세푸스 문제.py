28a8c93086ca7e59adccdfc5ac20acef4e60b432
Lim Yeeun <84782741+yeeunlim@users.noreply.github.com>
2026-02-20 16:24:26 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 3472 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,13 +1,11 @@
-import sys
 from collections import deque
-input = sys.stdin.readline
-
 n, k = map(int, input().split())
-queue = deque(range(1, n+1))
-progression = []
-
-while queue:
-    for _ in range(k-1):
-        queue.append(queue.popleft())
-    progression.append(queue.popleft())
-print(str(progression).replace('[', '<').replace(']', '>'))
+que = deque(str(i) for i in range(1, n+1))
+answer = []
+while que:
+    for i in range(1, k+1):
+        if i == k:
+            answer.append(que.popleft())
+        else:
+            que.append(que.popleft())
+print("<" + ", ".join(answer) + ">")
