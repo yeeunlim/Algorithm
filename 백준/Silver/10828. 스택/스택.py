677b5aed7742e373e16e401951351fc0fc9e701c
Lim Yeeun <84782741+yeeunlim@users.noreply.github.com>
2026-03-07 13:11:38 +0900

    [Silver IV] Title: 스택, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,25 +1,32 @@
 import sys
-N = int(sys.stdin.readline())
-stack = []
-for i in range(N):
-    command = sys.stdin.readline().split()
 
+n = int(sys.stdin.readline())
+
+stack = []
+
+for _ in range(n):
+    command = sys.stdin.readline().split()
+    
     if command[0] == 'push':
         stack.append(command[1])
+      
     elif command[0] == 'pop':
-        if not stack:
-            print(-1)
+        if len(stack) == 0:
+            print("-1")
         else:
             print(stack.pop())
+            
     elif command[0] == 'size':
         print(len(stack))
+        
     elif command[0] == 'empty':
-        if not stack:
-            print(1)
+        if len(stack) == 0:
+            print("1")
         else:
-            print(0)
+            print("0")
+            
     elif command[0] == 'top':
-        if not stack:
-            print(-1)
+        if len(stack) == 0:
+            print("-1")
         else:
             print(stack[-1])
