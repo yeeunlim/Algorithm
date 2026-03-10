24396d4d188c78ad7c08b0d38d7c9549c70a06a3
Lim Yeeun <84782741+yeeunlim@users.noreply.github.com>
2026-03-10 14:40:46 +0900

    [Silver V] Title: 날짜 계산, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1476. 날짜 계산/날짜 계산.py
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.py
@@ -1,11 +1,11 @@
-import sys
-input = sys.stdin.readline
-E, S, M = map(int, input().split())
-year = 1
-
+e, s, m = map(int, input().split())
+n = 1
 while True:
-    if ((year-E) % 15 == 0) and ((year-S) % 28 == 0) and ((year-M) % 19 == 0):
+    re_e = 15 if n % 15 == 0 else n % 15
+    re_s = 28 if n % 28 == 0 else n % 28
+    re_m = 19 if n % 19 == 0 else n % 19
+    
+    if re_e == e and re_s == s and re_m == m:
+        print(n)
         break
-    year += 1
-
-print(year)
+    n += 1
