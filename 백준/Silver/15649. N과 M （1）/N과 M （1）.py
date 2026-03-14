b60df823b4cdeff1a7f6667db2eb0cb9727475a2
Lim Yeeun <84782741+yeeunlim@users.noreply.github.com>
2026-03-14 17:25:01 +0900

    [Silver III] Title: N과 M (1), Time: 120 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,18 +1,22 @@
 import sys
 
-n, m = map(int, input().split())
+n, m = map(int, sys.stdin.readline().split())
 
-array = []  # 현재까지 선택된 요소들을 저장하는 리스트
-def backtracking():
-    if len(array) == m:  # 현재까지 선택된 요소의 개수가 m과 같다면
-        print(" ".join(map(str, array)))  # 현재 순열을 출력
-        return  # 함수를 종료하고 돌아감
+s = []  # 수열을 담을 스택
+visited = [False] * (n + 1)  # 숫자 사용 여부 확인
 
-    for i in range(1, n + 1):  # 1부터 n까지 모든 숫자에 대해
-        if i not in array:  # 현재 선택된 숫자들(array)에 포함되지 않은 숫자 i를 선택
-            array.append(i)  # 숫자 i를 선택 (추가)
-            backtracking()  # 재귀 호출을 통해 다음 숫자를 선택
-            array.pop()  # 선택했던 숫자 i를 제거 (백트래킹)
+def dfs():
+    # 수열의 길이가 m에 도달하면 출력 후 종료
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
 
+    for i in range(1, n + 1):
+        if not visited[i]:  # 아직 사용하지 않은 숫자인 경우
+            visited[i] = True
+            s.append(i)
+            dfs()           # 다음 숫자 선택을 위한 재귀 호출
+            s.pop()         # 탐색 후 스택에서 제거 (Backtrack)
+            visited[i] = False
 
-backtracking()
+dfs()
