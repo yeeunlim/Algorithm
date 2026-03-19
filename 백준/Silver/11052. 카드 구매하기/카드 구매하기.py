901107f76d2a1db1ac52a40f10fc7243f8896b0d
Lim Yeeun <84782741+yeeunlim@users.noreply.github.com>
2026-03-19 10:02:35 +0900

    [Silver I] Title: 카드 구매하기, Time: 160 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -0,0 +1,9 @@
+n = int(input())
+p = [0] + list(map(int, input().split()))
+dp = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    for k in range(1, i + 1):
+        dp[i] = max(dp[i], p[k] + dp[i - k])
+
+print(dp[n])
