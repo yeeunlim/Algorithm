9ea7986a36ab43e9f5b26ce49b73ff1c97d5625c
Lim Yeeun <84782741+yeeunlim@users.noreply.github.com>
2026-06-20 12:38:48 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,8 +1,19 @@
 def solution(brown, yellow):
-    width = int(brown * 0.5) - 2
-    height = 0
-    while width >= height:
-        if width * height == yellow:
-            return [width + 2, height + 2]
-        width -= 1
-        height += 1
+    # 2*w + 2*h - 4 = brown -> w + h = brown * 0.5 + 2
+    # (w - 2) * (h - 2) == yellow
+    # w * h == brown + yellow
+    # w >= h
+    total = int(brown*0.5) + 2
+    for h in range(3, total):
+        w = total - h
+        if (w - 2) * (h - 2) == yellow:
+            return [w, h]
+    # width = int(brown * 0.5) - 2
+    # height = 0
+    # while width >= height:
+    #     if width * height == yellow:
+    #         return [width + 2, height + 2]
+    #     width -= 1
+    #     height += 1
+    
+    
